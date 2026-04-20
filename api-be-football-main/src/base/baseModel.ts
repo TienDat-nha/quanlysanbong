653f1e4f396ae1cca39065e1bdb9fb4570bd518e
Duythai47 <duythai47@gmail.com>
2026-04-20 23:58:01 +0700

    fix

--- api-be-football-main/src/base/baseModel.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export type BaseDocument = mongoose.Document & {
-  createdAt?: Date;
-  updatedAt?: Date;
-};
--- api-be-football-main/src/constants/role.const.ts
+++
@@ -1,5 +0,0 @@
-export const ROLES: any = {
-  ADMIN: "ADMIN",
-  USER: "USER",
-  OWNER: "OWNER",
-};
